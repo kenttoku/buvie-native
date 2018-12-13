30502244ac7a05bb551e416ec3223044b12f49f4
Kent Tokunaga <kent@Kents-MacBook-Pro.local>
2018-12-13 16:09:04 -0500

    Add a toggle function to make debug easier

--- app/components/LandingPage/LandingPage.js
+++ app/components/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-native';
 
@@ -9,6 +9,14 @@ import LoginForm from './LoginForm';
 class LandingPage extends Component {
   state = {
     activeForm: 'registration'
+  }
+
+  toggleForm() {
+    let activeForm = 'login'
+    if (this.state.activeForm === 'login') {
+      activeForm = 'registration'
+    }
+    this.setState({activeForm})
   }
 
   render() {
@@ -23,6 +31,10 @@ class LandingPage extends Component {
       <View>
         {activeForm === 'registration' && <RegistrationForm />}
         {activeForm === 'login' && <LoginForm />}
+        <Button
+          title="Toggle Form"
+          onPress={() => this.toggleForm()}
+        />
       </View>
     );
   }
